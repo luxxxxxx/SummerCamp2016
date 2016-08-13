bf09ab239c1c0e1d23aa4268b9704bd8c02d35de
刘文栖 <974820947@qq.com>
2016-08-13 10:38:08 +0800

    plugin

---
+++ lesson_plugin.js
@@ -0,0 +1,20 @@
+var slider = (function () {
+    //定义一些属性
+    var per = ...
+    //定义辅助函数
+    function function_name(argument) {
+        // body...
+    }
+    return {
+        //拥有的一些方法
+        pre: function (argument) {
+            // body...
+        },
+        next: function (argument) {
+            // body...
+        }
+    }
+})();
+//调用
+slider.pre();
+slider.next();
